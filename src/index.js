979a0c982d641f13a62227a4d38157d9fef8af6c
prakashrawat1 <85007167+prakashrawat1@users.noreply.github.com>
2021-11-02 11:40:55 +0530

    demo

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import Rout from './Router';
 // import {BrowserRouter} from "react-router-dom";
-import Count  from './ProjectC/TimeCount';
-import Covid from './Covid'
+
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Main1 from './ProjectC/Main1';
 ReactDOM.render(
   <>
-{/* <Main1/> */}
-<Covid/>
+<Main1/>
   </>,
   document.getElementById('root')
 );
